df1531762f57383a1cecbd8ea946d47150c122a2
awalther <awalther@31858273-7fca-4d80-8e97-7185e5361c80>
2015-03-03 21:11:51 +0000

    some updates

--- get_viirs_data_day.sh
+++ get_viirs_data_day.sh
@@ -9,7 +9,7 @@ path=${args[2]}
  if date -v 1d > /dev/null 2>&1; then
   doy_dum=`expr ${doy} - 1` 
 
-  DAY_OBS=$(./shift_date.sh ${year}0101 +${doy_dum})
+  DAY_OBS=$( shift_date.sh ${year}0101 +${doy_dum})
   month=${DAY_OBS:4:2}
   day=${DAY_OBS:6:2}
 else
@@ -19,8 +19,8 @@ else
 fi
 echo $month
 echo $day
-
-cp peate_downloader.sh $path
+echo $HOME
+cp $HOME/ALGO/clavrx_scripts/trunk/peate_downloader.sh $path
 cd $path
  
 sh -c './peate_downloader.sh '$year'-'$month'-'$day'+00:00:00 '$year'-'$month'-'$day'+23:59:59 SVDNB GMTCO GITCO GDNBO SVM01 SVM02 SVM03 SVM04 SVM05 SVM06 SVM07 SVM08 SVM09 SVM10 SVM11 SVM12 SVM13 SVM14 SVM15 SVM16 IICMO SVI01'
