de540605339faa701b764d40e539c58de980c16f
dbotambekov <dbotambekov@31858273-7fca-4d80-8e97-7185e5361c80>
2016-04-20 19:22:49 +0000

    changed to the stable_release_2016a

--- trunk/install_clavrx_stable.sh
+++ trunk/install_clavrx_stable.sh
@@ -51,7 +51,7 @@ echo
 mkdir -p  $path
 cd $path
 
-svn checkout -q https://svn.ssec.wisc.edu/repos/cloud_team_clavrx/tags/stable_release_2015c ./
+svn checkout -q https://svn.ssec.wisc.edu/repos/cloud_team_clavrx/tags/stable_release_2016a ./
 
 
 cd dcomp
