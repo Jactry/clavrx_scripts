415601042999403117e3da3a619935e534398190
awalther <awalther@31858273-7fca-4d80-8e97-7185e5361c80>
2016-07-15 15:16:16 +0000

    fixed little bug for install_clavrx_trunk

--- trunk/install_clavrx_trunk.sh
+++ trunk/install_clavrx_trunk.sh
@@ -62,12 +62,12 @@ svn checkout -q https://svn.ssec.wisc.edu/repos/cloud_team_clavrx/trunk ./
 
 cd dncomp
 ./configure -hdf5root=$hdf5_path -with-ifort -hdf4root=${hdf4_path}
-
+make clean
 
 cd ../main_src
 cp level2_all_on.inc level2.inc
 ./configure -hdf5root=$hdf5_path -with-ifort  -hdf4root=${hdf4_path} -netcdflib=${netcdf_path}  -dncomp_dir=../dncomp/ -acha_dir=../cloud_acha/
-
+make clean
 
 
 if make; then 
